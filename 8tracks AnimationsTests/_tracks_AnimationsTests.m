8af4c88c10b9a0b181605099e1569be088656787
Chris O'Neil <cconeil5@gmail.com>
2013-12-22 21:04:34 -0500

    adding a bunch of files so someone can run this on their own phone or simulator

---
+++ 8tracks AnimationsTests/_tracks_AnimationsTests.m
@@ -0,0 +1,34 @@
+//
+//  _tracks_AnimationsTests.m
+//  8tracks AnimationsTests
+//
+//  Created by Chris O'Neil on 12/21/13.
+//  Copyright (c) 2013 Chris O'Neil. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _tracks_AnimationsTests : XCTestCase
+
+@end
+
+@implementation _tracks_AnimationsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
